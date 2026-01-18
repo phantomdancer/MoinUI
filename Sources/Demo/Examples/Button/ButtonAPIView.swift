e554ae65fd66bbcc1f6493de7f1d1156f8f13482
Finn <finn@example.com>
2026-01-18 13:03:04 +0800

    perf(ButtonAPIView): 将 LazyVStack 替换为 VStack 以提升性能

--- Sources/Demo/Examples/Button/ButtonAPIView.swift
+++ Sources/Demo/Examples/Button/ButtonAPIView.swift
@@ -167,7 +167,7 @@ struct ButtonAPIView: View {
             // 可滚动内容
             ScrollViewReader { proxy in
                 ScrollView {
-                    LazyVStack(alignment: .leading, spacing: Moin.Constants.Spacing.xl) {
+                    VStack(alignment: .leading, spacing: Moin.Constants.Spacing.xl) {
                         // API 分组
                         Text("API")
                             .font(.title3)
